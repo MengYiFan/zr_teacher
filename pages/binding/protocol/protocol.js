d903859b424b320120005f5a8bfab59e3fd9772f
mz_zml <mz_zml@163.com>
2018-10-03 16:30:51 +0800

    add protocol page before binding page

--- pages/binding/protocol/protocol.js
+++ pages/binding/protocol/protocol.js
@@ -1,78 +1,63 @@
-// pages/user/course/course.js
-import { getTeacherCourseList } from '../../../utils/api'
-
-const app = getApp()
-
+// pages/binding/notice/notice.js
 Page({
 
   /**
    * 页面的初始数据
    */
   data: {
-    courseList: {
-      rankState: false,
-      direction: 'row',
-      data: []
-    }
+    protocolImages: ['zr_01.jpg', 'zr_02.jpg', 'zr_03.jpg', 'zr_04.jpg', 'zr_05.jpg', 'zr_06.jpg', 'zr_07.jpg', 'zr_08.jpg']
   },
-
+  bindAgreeProtocolTap(e) {
+    wx.redirectTo({
+      url: '../../../pages/binding/index/binding',
+    })
+  },
   /**
    * 生命周期函数--监听页面加载
    */
   onLoad: function (options) {
-
+   
   },
 
   /**
    * 生命周期函数--监听页面初次渲染完成
    */
   onReady: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面显示
    */
   onShow: function () {
-    getTeacherCourseList({
-      data: {
-        userId: app.globalData.userId || wx.getStorageSync('userId')
-      },
-      success: res => {
-        if (res.code == '1000') {
-          this.setData({
-            ['courseList.data']: res.data
-          })
-        }
-      }
-    })
+
   },
 
   /**
    * 生命周期函数--监听页面隐藏
    */
   onHide: function () {
-  
+
   },
 
   /**
    * 生命周期函数--监听页面卸载
    */
   onUnload: function () {
-  
+
   },
 
   /**
    * 页面相关事件处理函数--监听用户下拉动作
    */
   onPullDownRefresh: function () {
-  
+
   },
 
   /**
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
+
   }
 })
--- pages/user/info/info.js
+++ pages/user/info/info.js
@@ -147,7 +147,7 @@ Page({
             }, 500)
           } else {
             wx.redirectTo({
-              url: '../../../pages/binding/index/binding',
+              url: '../../../pages/binding/protocol/protocol',
             })
           }
         }
@@ -238,13 +238,6 @@ Page({
    * 页面上拉触底事件的处理函数
    */
   onReachBottom: function () {
-  
-  },
-
-  /**
-   * 用户点击右上角分享
-   */
-  onShareAppMessage: function () {
   
   }
 })
